2fb061a483e537a2fc42f7b6e1977c33733c7fb7
Melanie Miu <melaniemiu@wifi-10-40-83-13.wifi.berkeley.edu>
2025-04-03 17:39:30 -0700

    pushing all my late hw

---
+++ Homework 7/calculator.py
@@ -0,0 +1,4 @@
+
+# Question 2
+
+import math_tools.py as math
---
+++ Homework 7/math_tools.py
@@ -0,0 +1,20 @@
+
+# Question 1
+
+import numpy as np
+
+a= 1
+b = 2
+
+print(np.add(a,b))
+
+print(np.subtract(a,b))
+
+print(np.multiply(a,b))
+
+print(np.divide(a,b))
+
+
+
+
+
